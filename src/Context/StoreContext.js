fe0ba6e196524b54d15181c017bcda2129c5964e
selva-dev3 <selvakumar.dev3@gmail.com>
2025-11-06 16:46:29 +0530

    Update StoreContext.js

--- src/Context/StoreContext.js
+++ src/Context/StoreContext.js
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 export const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = 'http://localhost:4000';
+    const url = 'https://food-delivery-mernstack.onrender.com';
     const [token, setToken] = useState("");
     const [food_list, setFood_list] = useState([])
     const addToCart = async (itemId) => {
